6fb7ab372ee92caa05d87dff390cd6ba8a7bee7f
Bassam Hashmi <bassamhashmi7@gmail.com>
2022-08-23 17:33:48 +0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { AuthProvider } from "./context/authContext";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
+  <React.StrictMode>
+    <AuthProvider>
+      <BrowserRouter>
         <App />
-    </React.StrictMode>
+      </BrowserRouter>
+    </AuthProvider>
+  </React.StrictMode>
 );
